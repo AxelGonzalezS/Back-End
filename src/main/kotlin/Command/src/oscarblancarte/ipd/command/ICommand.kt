a91b5bcbd6e13b7fb0f08123800d35c8431b5ebb
AxelGonzalezS <98973109+AxelGonzalezS@users.noreply.github.com>
2023-05-07 22:37:00 -0600

    codigo de los patrones de diseño

--- src/main/kotlin/Command/src/oscarblancarte/ipd/command/ICommand.kt
+++
@@ -1,8 +0,0 @@
-package Command.src.oscarblancarte.ipd.command
-
-import java.io.OutputStream
-
-interface ICommand {
-    val commandName: String?
-    fun execute(args: Array<String>, out: OutputStream?)
-}
